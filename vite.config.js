83da1818a7941b21ac0d8613c5e883ad94409f80
Swanagan Ray <65131940+sray91@users.noreply.github.com>
2026-09-18 10:37:28 -0400

    security: remove obfuscated loader hidden in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
-import { createRequire } from 'module';
 
-const require = createRequire(import.meta.url);
 
 // https://vite.dev/config/
 export default defineConfig({
